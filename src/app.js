1113b1744875d0681f307168d9971582cdd8dc8f
GentlemanMenace <kcrundquist@gmail.com>
2020-08-28 23:18:48 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,48 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+
+
+filterCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+
+    if (isChecked) {
+        for ( let i = 0; i < lis.length; i++ ) {
+            let li = lis[i];
+            if ( li.className === 'responded' ) {
+                li.style.display = '';
+            } else {
+                li.style.display = 'none';
+            }
+        }
+    } else {
+        for ( let i = 0; i < lis.length; i++ ) {
+            let li = lis[i];
+            li.style.display = '';
+        }
+    }
+});
 
 //This function creates an li element containing the HTML for the RSVP
 function createLI (text) {
     //creates li element
     const li = document.createElement('li');
-    li.textContent = text;
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
 
     //creates the confirmed label
     const label = document.createElement('label');
@@ -62,10 +98,31 @@ ul.addEventListener('click', (e) => {
         const button = e.target;
         const li = button.parentNode;
         const ul = li.parentNode;
+
+        //checks which button is clicked
         if (button.textContent === 'Remove') {
             ul.removeChild(li);
         } else if (button.textContent === 'Edit'){
-            console.log('edit');
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+
+            //displays text input to allow user to edit and removes span
+            li.insertBefore(input, span);
+            li.removeChild(span);
+
+            button.textContent = 'Save';
+        } else if (button.textContent === 'Save') {
+            const span = document.createElement('span');
+            const input = li.firstElementChild;
+            span.textContent = input.value;
+
+            //displays the edited text in the span and hides the text input
+            li.insertBefore(span, input);
+            li.removeChild(input);
+
+            button.textContent = 'Edit'
         }
     }
 });
